ad03e17fa883f31012688a65153e3a08cdc8a010
preanik24 <preanik24@gmail.com>
2025-12-27 15:07:12 +0300

    барарабабарарарарарар беререереререр

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import pygame as pg
+from operator import truediv
 
+import pygame
 
+pygame.init()
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("не знаю че писать")
 
+run = true
+while(run):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
